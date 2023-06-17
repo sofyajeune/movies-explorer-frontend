0eced6b4effa55d467edf5f872e0eaa337b51911
sofyajeune <sofya.b95@gmail.com>
2023-06-17 18:33:39 +0300

    profile validation fixed

--- movies/src/components/Profile/Profile.js
+++ movies/src/components/Profile/Profile.js
@@ -1,16 +1,16 @@
 import './Profile.css';
 import Layout from "../Layout/Layout";
-import React, { useEffect } from 'react';
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { api } from "../../utils/MainApi";
-import { useContext } from "react";
-import { CurrentUserContext } from "../../contexts/CurrentUserContext";
+import React, {useEffect} from 'react';
+import {useState} from "react";
+import {Link, useNavigate} from "react-router-dom";
+import {api} from "../../utils/MainApi";
+import {useContext} from "react";
+import {CurrentUserContext} from "../../contexts/CurrentUserContext";
 import MessagePopup from "../MessagePopup/MessagePopup";
 
 
-function Profile({ isLoggedIn, onOpenBurger, handleLogout }) {
-  const { state, updateState } = useContext(CurrentUserContext);
+function Profile({isLoggedIn, onOpenBurger, handleLogout}) {
+  const {state, updateState} = useContext(CurrentUserContext);
 
   const defaultUserName = 'User'; //currentUserName
   const defaultEmail = 'example@mail.ru'; //currentUserMail
@@ -18,19 +18,25 @@ function Profile({ isLoggedIn, onOpenBurger, handleLogout }) {
   const [userName, setUserName] = useState(defaultUserName);
   const [email, setEmail] = useState(defaultEmail);
 
-  let initialState = {
-    name: userName,
-    email: email,
-  };
+  const [initialState, setInitialState] = useState({
+    name: state.user.name,
+    email: state.user.email,
+  });
 
   const [isLoading, setLoading] = useState(true);
 
   const [errors, setErrors] = useState({});
   const [isValid, setIsValid] = useState(false);
+
   const [modifyEnabled, setModify] = useState(false);
   const [saveEnabled, setSave] = useState(false);
 
   const [isPopupVisible, setIsPopupVisible] = useState(false);
+
+  const checkEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
 
   const handleClosePopup = () => {
     setIsPopupVisible(false); // Close the popup
@@ -46,8 +52,30 @@ function Profile({ isLoggedIn, onOpenBurger, handleLogout }) {
   };
 
 
+  function testEmail(email) {
+    if (!checkEmail(email)) {
+      setErrors({
+        ...errors,
+        email: "Неправильная почта!",
+      });
+      return false;
+    } else {
+      setErrors({
+        ...errors,
+        email: null,
+      });
+      return true;
+    }
+  }
+
+  function testChange(username, email) {
+    console.log(username, initialState['name'])
+    return !(initialState['name'] === username &&
+      initialState['email'] === email);
+  }
+
   const handleChange = (e) => {
-    const { name, value } = e.target;
+    const {name, value} = e.target;
 
     setFormValue({
       ...formValue,
@@ -60,19 +88,23 @@ function Profile({ isLoggedIn, onOpenBurger, handleLogout }) {
     });
 
     setIsValid(e.target.closest('form').checkValidity());
+    let emailTest = true;
+    let changeTest = true;
+
   };
 
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setSave(false);
     api.setUserInfo(formValue).then((r) => {
       console.log(r);
       setModify(false);
-      setSave(false);
-      initialState = {
+      setInitialState({
         name: formValue.name,
         email: formValue.email,
-      };
+      })
+      console.log(initialState)
     }).catch((e) => {
       console.error(e);
       setIsPopupVisible(true);
@@ -110,13 +142,10 @@ function Profile({ isLoggedIn, onOpenBurger, handleLogout }) {
   }, []);
 
   useEffect(() => {
-    if (
-      initialState['name'] === formValue.name &&
-      initialState['email'] === formValue.email
-    ) {
-      setSave(false);
+    if (testEmail(formValue.email) && testChange(formValue.name, formValue.email)) {
+      setSave(true)
     } else {
-      setSave(true);
+      setSave(false)
     }
   }, [formValue]);
 
@@ -134,69 +163,69 @@ function Profile({ isLoggedIn, onOpenBurger, handleLogout }) {
   }
 
   return (
-    <Layout className="header header_white" isLoggedIn page={false} onOpenBurger={onOpenBurger}>
-      {isPopupVisible && (<MessagePopup onClose={handleClosePopup}
-        title={"Возникла ошибка"}
-        description={"Попробуйте повторить попытку позже"}
-      />)}
+    <Layout className="header header_white" isLoggedIn page={ false } onOpenBurger={ onOpenBurger }>
+      { isPopupVisible && (<MessagePopup onClose={ handleClosePopup }
+                                         title={ "Возникла ошибка" }
+                                         description={ "Попробуйте повторить попытку позже" }
+      />) }
       <section className="profile">
         <div className="profile__section">
           <div className="profile__container">
-            <h2 className="profile__greeting">{`Привет, ${userName}!`}</h2>
-            <form className="profile__form" onSubmit={handleSubmit}>
+            <h2 className="profile__greeting">{ `Привет, ${ initialState.name }!` }</h2>
+            <form className="profile__form" onSubmit={ handleSubmit }>
               <label className="profile__label">Имя
                 <input
                   // value={formValue.name}
-                  value={formValue.name}
+                  value={ formValue.name }
                   // defaultValue={ userName }
-                  disabled={!modifyEnabled}
+                  disabled={ !modifyEnabled }
                   type="text"
                   placeholder="name"
                   className="profile__input"
                   name="name"
                   required
                   id="name"
-                  onChange={handleChange}
+                  onChange={ handleChange }
                 />
-                <span id="name-error" className="profile__error">{errors.name}
+                <span id="name-error" className="profile__error">{ errors.name }
                 </span>
               </label>
               <label className="profile__label">Email
                 <input
                   // value={formValue.email || 'example@mail.ru'}
-                  value={formValue.email}
+                  value={ formValue.email }
                   // defaultValue={ email }
                   type="email"
-                  disabled={!modifyEnabled}
+                  disabled={ !modifyEnabled }
                   className="profile__input"
                   name="email"
                   required
                   id="email"
-                  onChange={handleChange}
+                  onChange={ handleChange }
                 />
-                <span id="email-error" className="profile__error">{errors.email}
+                <span id="email-error" className="profile__error">{ errors.email }
                 </span>
               </label>
               <>
                 <button
-                  className={`profile__button profile__change app__link ${modifyEnabled ? 'enabled' : null}`}
+                  className={ `profile__button profile__change app__link ${ modifyEnabled ? 'enabled' : null }` }
                   type="button"
-                  onClick={toggleEnableModify}
+                  onClick={ toggleEnableModify }
                 >
                   Редактировать
                 </button>
                 <Link
                   className="profile__button profile__exit app__link"
-                  type="button" onClick={() => {
-                    handleLogout();
-                    return navigate('/');
-                  }}
+                  type="button" onClick={ () => {
+                  handleLogout();
+                  return navigate('/');
+                } }
                 >
                   Выйти из аккаунта
                 </Link>
                 <button
-                  className={`profile__save-button_form ${saveEnabled ? "" : "profile__save-button_form_disabled"}`} //здесь надо менять класс в зависимости от того, нажата ли кнопка редактировать или нет, активное состояние - класс profile__save-button
-                  disabled={!saveEnabled}
+                  className={ `profile__save-button_form ${ saveEnabled ? "" : "profile__save-button_form_disabled" }` } //здесь надо менять класс в зависимости от того, нажата ли кнопка редактировать или нет, активное состояние - класс profile__save-button
+                  disabled={ !saveEnabled }
                   type="submit"
                 >
                   Сохранить
